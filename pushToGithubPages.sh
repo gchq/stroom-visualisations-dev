ae958d448fc49f1dcff633a8f38e50730fff04f0
at055612 <at055612@gmail.com>
2016-11-04 14:17:08 +0000

    Added comments to pushToGithubPages

--- pushToGithubPages.sh
+++ pushToGithubPages.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+#created local and remote gh-pages branch using
+#git subtree split --prefix war -b gh-pages
+#git push -f origin gh-pages:gh-pages
+
 git subtree push --prefix war origin gh-pages
