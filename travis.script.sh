0bd50f4626a3e2145f4ef0f28b4984b0441add2b
at055612 <at055612@gmail.com>
2019-02-22 13:27:28 +0000

    Add file hashes to release artifacts

--- travis.script.sh
+++ travis.script.sh
@@ -14,29 +14,52 @@ set -e
   NC='\033[0m' # No Colour 
 }
 
-if [ -n "$TRAVIS_TAG" ]; then
-  #Tagged commit so use that as our version, e.g. v3.2.0
-  VERSION="${TRAVIS_TAG}"
-fi
+create_file_hash() {
+  local -r file="$1"
+  local -r hash_file="${file}.sha256"
 
-#Dump all the travis env vars to the console for debugging
-echo -e "TRAVIS_BUILD_NUMBER: [${GREEN}${TRAVIS_BUILD_NUMBER}${NC}]"
-echo -e "TRAVIS_COMMIT:       [${GREEN}${TRAVIS_COMMIT}${NC}]"
-echo -e "TRAVIS_BRANCH:       [${GREEN}${TRAVIS_BRANCH}${NC}]"
-echo -e "TRAVIS_TAG:          [${GREEN}${TRAVIS_TAG}${NC}]"
-echo -e "TRAVIS_PULL_REQUEST: [${GREEN}${TRAVIS_PULL_REQUEST}${NC}]"
-echo -e "TRAVIS_EVENT_TYPE:   [${GREEN}${TRAVIS_EVENT_TYPE}${NC}]"
-echo -e "VERSION:             [${GREEN}${VERSION}${NC}]"
+  echo -e "Creating a SHA-256 hash for file ${GREEN}${file}${NC}"
+  sha256sum "${file}" > "${hash_file}"
+  echo -e "Created hash file ${GREEN}${hash_file}${NC}"
+}
 
-#Normal commit/PR/tag build
-extraBuildArgs=()
+generate_file_hashes() {
+  for file in "${TRAVIS_BUILD_DIR}"/stroom-content-artifacts/*.zip; do
+    create_file_hash "${file}"
+  done
+}
 
-if [ -n "$TRAVIS_TAG" ]; then
-  extraBuildArgs+=( ${VERSION} )
-  echo -e "extraBuildArgs:    [${GREEN}${extraBuildArgs[*]}${NC}]"
-fi
+main() {
 
-#Do the build
-./stroom-content-builder/buildImportBundle.sh "${extraBuildArgs[@]}"
+  if [ -n "$TRAVIS_TAG" ]; then
+    #Tagged commit so use that as our version, e.g. v3.2.0
+    local -r VERSION="${TRAVIS_TAG}"
+  fi
+
+  #Dump all the travis env vars to the console for debugging
+  echo -e "TRAVIS_BUILD_DIR:    [${GREEN}${TRAVIS_BUILD_DIR}${NC}]"
+  echo -e "TRAVIS_BUILD_NUMBER: [${GREEN}${TRAVIS_BUILD_NUMBER}${NC}]"
+  echo -e "TRAVIS_COMMIT:       [${GREEN}${TRAVIS_COMMIT}${NC}]"
+  echo -e "TRAVIS_BRANCH:       [${GREEN}${TRAVIS_BRANCH}${NC}]"
+  echo -e "TRAVIS_TAG:          [${GREEN}${TRAVIS_TAG}${NC}]"
+  echo -e "TRAVIS_PULL_REQUEST: [${GREEN}${TRAVIS_PULL_REQUEST}${NC}]"
+  echo -e "TRAVIS_EVENT_TYPE:   [${GREEN}${TRAVIS_EVENT_TYPE}${NC}]"
+  echo -e "VERSION:             [${GREEN}${VERSION}${NC}]"
+
+  #Normal commit/PR/tag build
+  local extraBuildArgs=()
+
+  if [ -n "$TRAVIS_TAG" ]; then
+    extraBuildArgs+=( "${VERSION}" )
+    echo -e "extraBuildArgs:    [${GREEN}${extraBuildArgs[*]}${NC}]"
+  fi
+
+  #Do the build
+  ./stroom-content-builder/buildImportBundle.sh "${extraBuildArgs[@]}"
+
+  generate_file_hashes
+}
+
+main "$@"
 
 exit 0
