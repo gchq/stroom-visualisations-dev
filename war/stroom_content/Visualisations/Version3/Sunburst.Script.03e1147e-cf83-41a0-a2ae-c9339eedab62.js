3267f96b63bbe88d2f77ee1266772b97be4a92db
stroomworks3401 <stroomworks3401@gmail.com>
2024-08-20 13:52:03 +0100

    Displays sunburst chart based on json

---
+++ war/stroom_content/Visualisations/Version3/Sunburst.Script.03e1147e-cf83-41a0-a2ae-c9339eedab62.js
@@ -0,0 +1,191 @@
+if (!visualisations) {
+    var visualisations = {};
+}
+
+//IIFE to prvide shared scope for sharing state and constants between the controller 
+//object and each grid cell object instance
+(function(){
+    var d3 = window.d3;
+    var commonFunctions = visualisations.commonFunctions;
+    var commonConstants = visualisations.commonConstants;
+
+    //Instantiated by Stroom
+    visualisations.Sunburst = function(containerNode) {
+
+        //Stroom creates a new iFrame for each visualisation so
+        //create a div for the gridded visualisation to be built in
+        var initialised = false;
+        if (containerNode){
+        var element = containerNode;
+        } else {
+        var element = window.document.createElement("div");
+        }
+        this.element = element;
+
+        // var grid = new visualisations.GenericGrid(this.element);
+        var color, canvas, svg, width, height, radius, partition, arc;
+
+        //Called by GenericGrid to create a new instance of the visualisation for each cell.
+        this.getInstance = function(containerNode) {
+            return new visualisations.Sunburst(containerNode);
+        };
+
+        //one off initialisation of all the local variables, including
+        //appending various static dom elements
+        var initialise = function() {
+            initialised = true;
+    
+            width = commonFunctions.gridAwareWidthFunc(true, containerNode, element);
+            height = commonFunctions.gridAwareHeightFunc(true, containerNode, element);
+            radius = Math.min(width, height) / 2;
+    
+            canvas = d3.select(element).append("svg")
+                .attr("width", width)
+                .attr("height", height);
+    
+            svg = canvas.append("g")
+                .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+            
+            color = d3.scale.category20c();
+            partition = d3.layout.partition()
+                .size([2 * Math.PI, radius])
+                .value(function(d) { return d.value; });
+    
+            arc = d3.svg.arc()
+                .startAngle(function(d) { return d.x; })
+                .endAngle(function(d) { return d.x + d.dx; })
+                .innerRadius(function(d) { return d.y; })
+                .outerRadius(function(d) { return d.y + d.dy; });
+        };
+
+        //called by Stroom to pass snapshots of the data as it gathers the query results
+        //context - an object containing any shared context between Stroom and the visualisation,
+        //          e.g. a common colour scale could be used between multiple visualisations
+        //settings - the object containing all the user configurable settings for the visualisation,
+        //           e.g. showLabels, displayXAxis, etc.
+        //d - the object tree containing all the data. Always contains all data currently available
+        //    for a query.
+        this.setData = function(context, settings, d) {
+            if (context) {
+                if (context.color) {
+                  color = context.color;
+                } else {
+                  context.color = color;
+                }
+            }
+      
+            if (settings){
+              //Inspect settings to determine which axes to synch, if any.
+              //Change the settings property(s) used according to the vis
+              var synchedFields = [];
+              if (commonFunctions.isTrue(settings.synchXAxis)){
+                  synchedFields.push(0);
+              }
+              if (commonFunctions.isTrue(settings.synchYAxis)){
+                  synchedFields.push(1);
+              }
+      
+              if (commonFunctions.isTrue(settings.synchSeries)) {
+                  //series are synched so setup the colour scale domain and add it to the context
+                  //so it can be passed to each grid cell vis
+                  context.color = colour;
+              } else {
+                  //ensure there is no colour scale in the context so each grid cel vis can define its own
+                  delete context.color;
+              }
+            
+              //Get grid to construct the grid cells and for each one call back into a
+              //new instance of this to build the visualisation in the cell
+              //The last array arg allows you to synchronise the scales of fields
+            //   grid.buildGrid(context, settings, data, this, commonConstants.transitionDuration, synchedFields);
+              this.setDataInsideGrid(context, settings, d)
+            }
+        }
+
+        //called by Stroom to instruct the visualisation to redraw itself in a resized container
+        this.resize = function() {
+            //get GenericGrid to reszie the whole and to resize each grid cell visualisation
+            grid.resize();
+        };
+
+        //Called by GenericGrid to establish which position in the values array
+        //(or null if it is the series key) is used for the legend.
+        this.getLegendKeyField = function() {
+            
+        };
+    
+
+        //called by GenercGrid to build/update a visualisation inside a grid cell
+        //context - an object containing any shared context between Stroom and the visualisation,
+        //          e.g. a common colour scale could be used between multiple visualisations.
+        //          Also can be used by the grid to pass state down to each cell
+        //settings - the object containing all the user configurable settings for the visualisation,
+        //           e.g. showLabels, displayXAxis, etc.
+        //data - the object tree containing all the data for that grid cell. Always contains all data 
+        //       currently available for a query.
+        this.setDataInsideGrid = function(context, settings, data) {
+            if (!initialised){
+                initialise();
+            }
+        
+            // If the context already has a colour set then use it
+            if (context) {
+                visContext = context;
+                if (context.color) {
+                    colour = context.color;
+                }
+            }
+    
+            if (data) {
+                update(500, data.values[0]);
+            }
+        };
+
+        var update = function(duration, data) {
+            svg.selectAll("*").remove();
+
+            var nodes = partition.nodes(data);
+
+            var path = svg.selectAll("path")
+                .data(nodes)
+                .enter().append("path")
+                .attr("display", function(d) { return d.depth ? null : "none"; }) // hide inner ring
+                .attr("d", arc)
+                .style("stroke", "#fff")
+                .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+                .style("fill-rule", "evenodd")
+                .each(function(d) { d._current = d; }) // store the initial angles
+                .on("click", click);
+
+            path.append("title")
+                .text(function(d) { return d.name + "\n" + d.value; });
+
+            function click(d) {
+                svg.transition()
+                    .duration(duration)
+                    .tween("scale", function() {
+                        var xd = d3.interpolate(svg.x.domain(), [d.x, d.x + d.dx]),
+                            yd = d3.interpolate(svg.y.domain(), [d.y, 1]),
+                            yr = d3.interpolate(svg.y.range(), [d.y ? 20 : 0, radius]);
+                        return function(t) { svg.x.domain(xd(t)); svg.y.domain(yd(t)).range(yr(t)); };
+                    })
+                    .selectAll("path")
+                    .attrTween("d", function(d) { return function() { return arc(d); }; });
+            }
+
+            commonFunctions.addDelegateEvent(svg, "mouseover", "path", inverseHighlight.makeInverseHighlightMouseOverHandler(null, nodes.types, svg, "path"));
+            commonFunctions.addDelegateEvent(svg, "mouseout", "path", inverseHighlight.makeInverseHighlightMouseOutHandler(svg, "path"));
+            commonFunctions.addDelegateEvent(svg, "click", "path", inverseHighlight.makeInverseHighlightMouseClickHandler(svg, "path"));
+
+            commonFunctions.addDelegateEvent(svg, "mousewheel", "path", inverseHighlight.makeInverseHighlightMouseOutHandler(svg, "path"));
+            commonFunctions.addDelegateEvent(svg, "mousedown", "path", inverseHighlight.makeInverseHighlightMouseOutHandler(svg, "path"));
+        };
+
+        // Used to provide the visualisation's D3 colour scale to the grid
+        this.getColourScale = function() {
+            return color;
+        };
+
+    };
+
+}());
